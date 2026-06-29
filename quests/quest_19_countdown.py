5f20d1b9873f87db7d90e97b06addea0c23787a0
ntakirutimanakelvin <k.ntakiruti@alustudent.com>
2026-06-29 08:17:43 -0700

    countdown

--- quests/quest_19_countdown.py
+++ quests/quest_19_countdown.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+# This program counts down from 10 to 1 using a for loop,
+# then prints "Blastoff!" to simulate a rocket launch.
 for count in range(10, 0, -1):
     print(count)
 
