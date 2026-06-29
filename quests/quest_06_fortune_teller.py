95751debb3c90511eb90bbd6019b50a40a34bfef
uwase mireille <m.ndayiragi@alustudent.com>
2026-06-29 21:42:21 +0200

    Delete quests/quest_06_fortune_teller.py

--- quests/quest_06_fortune_teller.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-user_name = input("What is your name? ")
-user_quest = input("What is your quest? ")
-print(f"Greetings, {user_name}! Your quest to {user_quest} has been accepted.")
