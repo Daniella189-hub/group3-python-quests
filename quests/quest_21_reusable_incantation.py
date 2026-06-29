b7f4fe55a25056f3d2f47d03b60bcc541666862c
uwase mireille <m.ndayiragi@alustudent.com>
2026-06-29 21:45:02 +0200

    Delete quests/quest_21_reusable_incantation.py

--- quests/quest_21_reusable_incantation.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-# This program defines a function that prints a welcome message
-# and calls it three times.
-def greet_adventurer():
-    print("Welcome, Adventurer!")
-
-greet_adventurer()
-greet_adventurer()
-greet_adventurer()
